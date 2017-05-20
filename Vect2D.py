2a8c708819f319f5659dcc6dab367028620aa5bb
Phogly <adrien.meilac@ensae.fr>
2017-05-21 00:36:41 +0200

    Update Vect2D.py

--- Vect2D.py
+++ Vect2D.py
@@ -1,12 +1,17 @@
+## Fichier Vect2D.py
+## Ce fichier contient une classe eponyme et des methodes permettant d'effectuer les operations de bases sur des vecteurs à deux dimensions.
 from math import*
 
-class vect2D:    
+class vect2D:
+    '''Fourni des vecteurs à deux dimensions'''
     def __init__(self, x = 0, y = 0):
+        '''constructeur d'un vecteur'''
         self.x = x
         self.y = y
         
-## Addition de vecteurs
+    # Addition de vecteurs
     def __iadd__(self, vecteur):
+        '''Gestion de +='''
         nVecteur = vect2D()
         nVecteur.x = self.x
         nVecteur.y = self.y
@@ -14,10 +19,12 @@ class vect2D:
         nVecteur.y += vecteur.y
         return nVecteur
     def __add__(self, vecteur):
+        '''Gestion de l'addition de deux vecteurs'''
         nVecteur = self
         nVecteur += vecteur
         return nVecteur
     def __isub__(self, vecteur):
+        '''Gestion de -='''
         nVecteur = vect2D()
         nVecteur.x = self.x
         nVecteur.y = self.y
@@ -26,12 +33,14 @@ class vect2D:
         return nVecteur
         return nVecteur
     def __sub__(self, vecteur):
+        '''Gestion de la soustraction de deux vecteurs'''
         nVecteur = self
         nVecteur -= vecteur
         return nVecteur
         
-## Multiplication par un scalaire
+    # Multiplication par un scalaire
     def __imul__(self, scalaire):
+        '''Gestion de la multiplication d'un vecteur par un scalaire via l'operateur *='''
         nVecteur = vect2D()
         nVecteur.x = self.x
         nVecteur.y = self.y
@@ -39,15 +48,17 @@ class vect2D:
         nVecteur.y *= scalaire
         return nVecteur
     def __mul__(self, scalaire):
+        '''Gestion de la multiplication d'un vecteur par un scalaire'''
         nVecteur = vect2D()
         nVecteur.x = self.x
         nVecteur.y = self.y
         nVecteur *= scalaire
         return nVecteur
     def __rmul__(self, scalaire):
+        '''Gestion de la multiplication d'un vecteur par un scalaire'''
         return self * scalaire
 
-## Affichage d'un vecteur
+    ## Affichage d'un vecteur
     def __str__(self):
         return "[{},{}]".format(self.x,self.y)
     def __repr__(self):
@@ -65,7 +76,9 @@ class vect2D:
         
         
 def p_scal(vecteur1, vecteur2):
+    '''Calcule le produit scalaire de deux vecteurs'''
     return vecteur1.x * vecteur2.x + vecteur1.y * vecteur2.y
     
-def projection(vecteur1, vecteur2): #projection du vecteur "vecteur1" sur le vecteur "vecteur2"
+def projection(vecteur1, vecteur2): 
+    '''Calcule la projection du vecteur 1 sur l'axe dirigé par le vecteur 2'''
     return p_scal(vecteur1, vecteur2.normalise())*vecteur2.normalise()
