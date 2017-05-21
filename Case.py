3ccae08450ecc62cd7ad2118933c49d999abef95
Super-Caneton <kevin.lam@ensae.fr>
2017-05-21 18:17:30 +0200

    Update Case.py

--- Case.py
+++ Case.py
@@ -9,15 +9,15 @@ import Variables as Var
 class case:
     def __init__(self, pos = vect2D(), dim = 0, type = 0, score = 0, canvas = "", color = "ivory", explore = False, grille = False):
         self.pos = pos          # Position de la case
-        self.dim = dim          # Dimension de la case
+        self.dim = dim          # Dimension de la case en pixels
         self.type = type        # Type de la case
         self.score = score      # Distance du plus court chemin à la sortie
-        self.canvas = canvas    # ??
+        self.canvas = canvas    # Le Canevas sur lequel on dessine
         self.color = color      # Couleur de la case
         self.explore = explore  # Boleen qui indique si la case a ete ou non parcourue
-        self.grille = grille    # ??
-        self.id = canvas.create_rectangle(0, 0, dim, dim, fill = color, outline = color) # representation graphique de la case ??
-        self.canvas.move(self.id, pos.x * dim, pos.y * dim)
+        self.grille = grille    # Affiche les bords ou non pour la grille
+        self.id = canvas.create_rectangle(0, 0, dim, dim, fill = color, outline = color) # representation graphique de la case
+        self.canvas.move(self.id, pos.x * dim, pos.y * dim) #Positionnement de la case
     
     def raffraichir(self) :
         '''raffraichit la couleur d'une case'''
@@ -39,7 +39,7 @@ class case:
         return
     
     def degrade(self, fg, bg, maxd):
-        '''Change la couleur en faisant un dégradé entre fg et bg selon la distance du plus court chemin et de la distance maximum ??'''
+        '''Change la couleur en faisant un dégradé entre fg et bg selon la distance du plus court chemin et de la distance maximum'''
         def blend(i,fg,bg) :
             ''' interpolation linéaire de la couleur'''
             return (floor((1 - i) * fg[0] + i * bg[0]), int((1 - i) * fg[1] + i * bg[1]), int((1 - i) * fg[2] + i * bg[2]))
