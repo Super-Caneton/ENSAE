504a2b2f967977c403e4caec6b42daa6656d7ede
Phogly <adrien.meilac@ensae.fr>
2017-05-21 21:17:15 +0200

    Update Texte.py

--- Texte.py
+++ Texte.py
@@ -5,14 +5,14 @@ import numpy as np
 from Vect2D import *
 import Variables as Var
 
-class texte:
+class texte :
     '''permet de definir des mots sur le graphique avec une position précise'''
     def __init__(self, pos = vect2D(), mot = "", canvas = "", color = "black"):
-        self.pos = pos          # La position du centre du texte
-        self.mot = mot          # Le mot(ou la phrase) que l'on doit afficher
-        self.canvas = canvas    # Le Canevas sur lequel on dessine
+        self.pos = pos          # La position du centre du texte sur le terrain
+        self.mot = mot          # Texte à afficher
+        self.canvas = canvas    # Pointe vers le terrain sur leque on dessine
         self.color = color      # La couleur du texte
-        self.id = canvas.create_text(pos.x, pos.y, text = mot) #Représentation graphique
+        self.id = canvas.create_text(pos.x, pos.y, text = mot) # Réprésentation graphique
         
     def raffraichir(self):
         self.canvas.coords(self.id, self.pos.x, self.pos.y)
@@ -30,7 +30,7 @@ def init_texte(terrain):
             Var.Ttexte[y, x] = texte(vect2D(x, y) * Var.dimCase + vect2D(1, 1) * (Var.dimCase / 2), canvas = terrain)
     return
     
-def cacher_texte() :
+def cacher_texte():
     '''Permet de masquer la distance notée sur chaque case en mettant un mot vide'''
     for x in range(Var.largeur):
         for y in range(Var.hauteur):
